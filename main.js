f1ce2720fd99542ae4bb0cb343c5a6aa516cfb36
devawais <getawaisdeveloper@gmail.com>
2022-06-15 02:35:19 +0500

    Homepage updated

--- main.js
+++ main.js
@@ -39,6 +39,14 @@ $(document).ready(function(){
     $(setupColumns);
 })(jQuery);
 
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
 
 
 

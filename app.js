0f190b4f582ea8435c84f124a6d70aea79f1dcde
Murad <murad.jaddoon@gmail.com>
2022-06-16 13:03:40 +0500

    added on click functionality on header to open or close subheader

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const humberger = document.querySelector(".humberger");
 const headerLinks = document.querySelector(".header-links");
+const link1 = document.querySelector(".link1");
+const link2 = document.querySelector(".link2");
+const link3 = document.querySelector(".link3");
+const subHeader1 = document.querySelector(".sub-header-1");
+const subHeader2 = document.querySelector(".sub-header-2");
+const subHeader3 = document.querySelector(".sub-header-3");
 
 humberger.addEventListener("click", () => {
   headerLinks.classList.toggle("open");
+});
+link1.addEventListener("click", () => {
+  subHeader1.classList.toggle("open-sub-header");
+});
+link2.addEventListener("click", () => {
+  subHeader2.classList.toggle("open-sub-header");
+});
+link3.addEventListener("click", () => {
+  subHeader3.classList.toggle("open-sub-header");
 });

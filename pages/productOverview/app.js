ceef6340aaee59ff24521ae01613014e511c928c
Murad <murad.jaddoon@gmail.com>
2022-06-16 13:16:42 +0500

    updated product over viiew header with subheader

--- pages/productOverview/app.js
+++ pages/productOverview/app.js
@@ -1,6 +1,1 @@
-const humberger = document.querySelector(".humberger");
-const headerLinks = document.querySelector(".header-links");
 
-humberger.addEventListener("click", () => {
-  headerLinks.classList.toggle("open");
-});
